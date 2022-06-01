01e7e946213db5a500351139d4581a01e38636ac
Erik <erikilham2001@gmail.com>
2022-06-01 23:04:54 +0700

    criteria page

---
+++ app/src/main/java/com/capstone/healthyplate/CriteriaActivity.kt
@@ -0,0 +1,11 @@
+package com.capstone.healthyplate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CriteriaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_criteria)
+    }
+}
