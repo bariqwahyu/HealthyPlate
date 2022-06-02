75fb19961d9775f7f25d5c4db6b40e139c8e27c9
Bariq Wahyu <bariqwahyu09@gmail.com>
2022-06-03 00:33:13 +0700

    criteria Activity

--- app/src/main/java/com/capstone/healthyplate/CriteriaActivity.kt
+++ app/src/main/java/com/capstone/healthyplate/CriteriaActivity.kt
@@ -1,11 +1,41 @@
 package com.capstone.healthyplate
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.capstone.healthyplate.databinding.ActivityCriteriaBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class CriteriaActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCriteriaBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_criteria)
+        binding = ActivityCriteriaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+
+    }
+
+    private fun sendEmailVerification() {
+        val firebaseAuth = FirebaseAuth.getInstance()
+        val firebaseUser = firebaseAuth.currentUser
+
+
+        firebaseUser!!.sendEmailVerification()
+            .addOnSuccessListener {
+                Toast.makeText(this@CriteriaActivity, "Instructions Sent...", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(
+                    this@CriteriaActivity,
+                    "Failed to send due to " + e.message,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
     }
 }
