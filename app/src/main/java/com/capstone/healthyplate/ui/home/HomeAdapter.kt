caaa40bf5d85b7c82dd23ca632d3eca843222aeb
Erik <erikilham2001@gmail.com>
2022-06-08 23:51:36 +0700

    home rv update

--- app/src/main/java/com/capstone/healthyplate/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/capstone/healthyplate/ui/home/HomeAdapter.kt
@@ -1,20 +1,25 @@
 package com.capstone.healthyplate.ui.home
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.capstone.healthyplate.R
 
 class HomeAdapter (private val homeList : ArrayList<Home>) : RecyclerView.Adapter<HomeAdapter.MyViewHolder>() {
 
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("Not yet implemented")
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_home, parent, false)
+        return MyViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentitem = homeList[position]
+
+        holder.ingredients.text = currentitem.ingredients
     }
 
     override fun getItemCount(): Int {
@@ -23,6 +28,6 @@ class HomeAdapter (private val homeList : ArrayList<Home>) : RecyclerView.Adapte
     }
 
     class MyViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
-        val ingredients : TextView = itemView.findViewById(R.id.)
+        val ingredients : TextView = itemView.findViewById(R.id.txt_object)
     }
 }
