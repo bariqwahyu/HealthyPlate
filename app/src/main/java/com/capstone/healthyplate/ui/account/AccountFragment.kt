2b5f337a4d1e79d861d09aec838e63cda8c2fe7a
Erik <erikilham2001@gmail.com>
2022-06-08 20:09:42 +0700

    home rv

--- app/src/main/java/com/capstone/healthyplate/ui/account/AccountFragment.kt
+++ app/src/main/java/com/capstone/healthyplate/ui/account/AccountFragment.kt
@@ -14,7 +14,6 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
-import java.io.File
 
 class AccountFragment : Fragment() {
 
