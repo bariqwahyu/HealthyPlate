7b30852c4bfb704196ad3b21ac1b4ac278a8594b
Bariq Wahyu <bariqwahyu09@gmail.com>
2022-06-13 09:02:16 +0700

    remove unused

--- app/src/main/java/com/capstone/healthyplate/model/GeneratedRecipe.kt
+++
@@ -1,9 +0,0 @@
-package com.capstone.healthyplate.model
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class GeneratedRecipe(
-    val foodName: String
-) : Parcelable
